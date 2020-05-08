0a1e1ba3fc26c6c38e3aa2007c2f1fff996bd6a1
xyl <416319808@qq.com>
2020-05-08 23:38:56 +0800

    账户服务

--- services/accounts.go
+++ services/accounts.go
@@ -1,1 +1,45 @@
 package services
+
+import "time"
+
+type AccountService interface {
+	CreateAccount(dto AccountCreatedDTO) (*AccountDTO, error)
+	Transfer(dto AccountTransferDTO) (TransferedStatus, error)
+	StoreValue(dto AccountTransferDTO) (TransferedStatus, error)
+	GetEnvelopeAccountByUserId(userId string) *AccountDTO
+}
+
+// TradeParticipator 账户交易参与者
+type TradeParticipator struct {
+	AccountNo string
+	UserId    string
+	UserName  string
+}
+
+// AccountTransferDTO 账户转账
+type AccountTransferDTO struct {
+	TradeNo     string
+	TradeBody   TradeParticipator
+	TradeTarget TradeParticipator
+	AmountStr   string
+	ChangeType  ChangeType
+	ChangeFlag  ChangeFlag
+	Decs        string
+}
+
+// AccountCreatedDTO 账户创建
+type AccountCreatedDTO struct {
+	UserId       string
+	UserName     string
+	AccountName  string
+	AccountType  int
+	CurrencyType string
+	Amount       string
+}
+
+// AccountDTO 账户信息
+type AccountDTO struct {
+	AccountCreatedDTO
+	AccountNo string
+	CreatedAt time.Time
+}
---
+++ services/accounts_consts.go
@@ -0,0 +1,41 @@
+package services
+
+// TransferedStatus 转账状态
+type TransferedStatus int8
+
+const (
+	// 转账失败
+	TransferedStatusFailure TransferedStatus = -1
+	// 余额不足
+	TransferedStatusSufficientFunds TransferedStatus = 0
+	// 转账成功
+	TransferedStatusSuccess TransferedStatus = 1
+)
+
+// ChangeType 转账类型：0创建账户 >=1进账 <=-支出
+type ChangeType int8
+
+const (
+	// 账户创建
+	AccountCreated ChangeType = 0
+	// 存储值
+	AccountStoreValue ChangeType = 1
+	// 红包资金的支出
+	EnvelopeOutgoing ChangeType = -2
+	// 红包资金的收入
+	EnvelopeIncoming ChangeType = 2
+	// 红包资金的退款
+	EnvelopeExpiredRefund ChangeType = 3
+)
+
+// ChangeFlag 资金交易变化标识
+type ChangeFlag int8
+
+const (
+	// 创建账户
+	FlagAccountCreated ChangeFlag = 0
+	// 支出
+	FlagTransferOut ChangeFlag = -1
+	// 收入
+	FlagTransferIn ChangeFlag = 1
+)
