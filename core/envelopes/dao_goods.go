20614fa9557052f96d152a0a8b5590a63a99b077
xyl <416319808@qq.com>
2020-05-19 20:59:57 +0800

    '更新红包和数量'

--- core/envelopes/dao_goods.go
+++ core/envelopes/dao_goods.go
@@ -1,1 +1,44 @@
 package envelopes
+
+import (
+	"github.com/shopspring/decimal"
+	"github.com/sirupsen/logrus"
+	"github.com/tietang/dbx"
+)
+
+type RedEnvelopeGoodsDao struct {
+	runner dbx.TxRunner
+}
+
+// Insert 写入
+func (dao *RedEnvelopeGoodsDao) Insert(goods RedEnvelopeGoods) (int64, error) {
+	res, err := dao.runner.Insert(goods)
+	if err != nil {
+		return 0, err
+	}
+	return res.LastInsertId()
+}
+
+// GetOne 根据红包编号查询红包
+func (dao *RedEnvelopeGoodsDao) GetOne(envelopeNo string) *RedEnvelopeGoods {
+	good := &RedEnvelopeGoods{EnvelopeNo: envelopeNo}
+	ok, err := dao.runner.GetOne(good)
+	if err != nil || !ok {
+		logrus.Error(err)
+		return nil
+	}
+	return good
+}
+
+// UpdateBalance 更新红包数量和余额
+func (dao *RedEnvelopeGoodsDao) UpdateBalance(envelopeNo string, amount decimal.Decimal) (int64, error) {
+	sql := "update red_envelope_goods set remain_amount=remain_amount-CAST(? AS DECIMAL(30,6)),remain_quantity=remain_quantity-1" +
+		" where envelope_no=? " +
+		// 乐观锁代替事务行锁
+		"and remain_quantity>0 and remain_amount >= CAST(? AS DECIMAL(30,6))"
+	res, err := dao.runner.Exec(sql, amount.String(), envelopeNo, amount.String())
+	if err != nil {
+		return 0, err
+	}
+	return res.RowsAffected()
+}
