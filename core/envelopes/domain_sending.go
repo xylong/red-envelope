36c9b01394ba4f843f448fa718d0e7f97e58331f
xyl <416319808@qq.com>
2020-06-10 07:58:53 +0800

    发红包

--- core/envelopes/domain_sending.go
+++ core/envelopes/domain_sending.go
@@ -1,1 +1,21 @@
 package envelopes
+
+import (
+	"fmt"
+	"path"
+	"red-envelope/infra/base"
+	"red-envelope/services"
+)
+
+// SendOut 发红包
+func (d *goodsDomain) SendOut(dto services.RedEnvelopeGoodsDTO) (activity *services.RedEnvelopeActivity, err error) {
+	// 创建红包
+	d.Create(dto)
+	// 创建活动
+	activity = new(services.RedEnvelopeActivity)
+	domain := base.GetEnvelopeActivityDomain()
+	link := base.GetEnvelopeActivityLink()
+	activity.Link = path.Join(domain, link, d.EnvelopeNo)
+	// 保存红包
+	// 支付
+}
--- infra/base/props.go
+++ infra/base/props.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/tietang/props/kvs"
 	"red-envelope/infra"
+	"sync"
 )
 
 var props kvs.ConfigSource
@@ -20,4 +21,33 @@ type PropsStart struct {
 func (p *PropsStart) Init(ctx infra.StarterContext) {
 	props = ctx.Props()
 	fmt.Println("初始化配置...")
+}
+
+type SystemAccount struct {
+	AccountNo   string
+	AccountName string
+	UserId      string
+	Username    string
+}
+
+var systemAccount *SystemAccount
+var systemAccountOnce sync.Once
+
+func getSystemAccount() *SystemAccount {
+	systemAccountOnce.Do(func() {
+		systemAccount = new(SystemAccount)
+		err := kvs.Unmarshal(Props(), systemAccount, "system.account")
+		if err != nil {
+			panic(err)
+		}
+	})
+	return systemAccount
+}
+
+func GetEnvelopeActivityLink() string {
+	return Props().GetDefault("envelope.link", "/v1/envelope/link")
+}
+
+func GetEnvelopeActivityDomain() string {
+	return Props().GetDefault("envelope.domain", "http://localhost")
 }
